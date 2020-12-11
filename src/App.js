e522cc97d6296c543490583ce486ba9873c10270
Akash Immanuel D <akashimmanuel2@gmail.com>
2020-12-11 12:48:36 +0530

    inital changes in sidenav and navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Nav from "./components/Nav";
 import "./App.css";
-import SideNav from "./components/SideNav";
 
 export default function App() {
   return (
     <>
-      <div className="p-grid">
-        <div className="p-col-12">
-          <Nav />
-        </div>
-      </div>
+      <Nav />
     </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Menubar } from "primereact/menubar";
+import SideNav from "./SideNav";
 
 export default function Nav() {
   return (
     <>
       <Menubar />
+      <SideNav />
     </>
   );
 }
