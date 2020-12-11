2a0ecc2ad830ad0e5e6613f2e845b292ea4bbf08
Akash Immanuel D <akashimmanuel2@gmail.com>
2020-12-11 11:27:22 +0530

    refactor ui

--- src/App.js
+++ src/App.js
@@ -1,54 +1,5 @@
-import React from 'react';
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-import { Provider } from "react-redux";
-import { applyMiddleware } from "redux";
-
-import reducer from "./reducer";
-import { createStore } from "redux";
-
-import NavBar from "./components/Nav";
-
-import AuthRoute from "./components/AuthRoute";
-
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/Login";
-
-import { appMiddleware } from "./middlewares/app";
-import { apiMiddleware } from "./middlewares/core";
-
-const createStoreWithMiddleware = applyMiddleware(
-  appMiddleware,
-  apiMiddleware
-)(createStore);
-
-const store = createStoreWithMiddleware(reducer);
-
-const IndexPage = () => (
-  <>
-    <h3>Welcome to the App</h3>
-  </>
-);
+import React from "react";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <AuthRoute path="/login" type="guest">
-              <LoginPage />
-            </AuthRoute>
-            <AuthRoute path="/home" render={HomePage} type="private" />
-            {/* <AuthRoute path="/my-account" type="private">
-              <MyAccount />
-            </AuthRoute> */}
-            <Route path="/" render={IndexPage} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
-  );
+  return <div>hello</div>;
 }
