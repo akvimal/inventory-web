a218b2ff1e16787ec18c988c6617d5d508fbace4
vimalesh11 <vimalesh1197@yahoo.com>
2020-12-16 10:13:08 +0530

    search styling updated

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Table from "../components/table";
 import Band from "../components/band";
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
 
 export default function search() {
   return (
     <>
       <div id="scroll-cards">
-        <div className="mt-3 ml-4 mr-4">search</div>
+        <div className=" search-text mt-3 ml-4 mr-4"> Search By <br/>
+          <Dropdown className="dropdown-value" placeholder="Machine ID" />
+          <InputText className="input-value"/>
+        </div>
       </div>
       <div id="table">
         <div className=" mt-3 ml-4 mr-4">
