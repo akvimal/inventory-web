22558de1c2055413e82610cf88ceabab8eccb24d
vimalesh11 <vimalesh1197@yahoo.com>
2020-12-17 13:52:48 +0530

    data from api and buttons active

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,9 +2,20 @@ import React from "react";
 import { Sidebar } from "primereact/sidebar";
 import { Button } from "primereact/button";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function SideNav() {
+  const location = useLocation();
+  const paths = [
+    { path: "device" },
+    { path: "company" },
+    { path: "search" },
+  ];
+  const active = paths.map((d) => {
+    return location.pathname.split("/")[1] === d.path
+      ? "sidenav-buttons-active"
+      : "sidenav-buttons";
+  });
   return (
     <>
       <Sidebar visible={true} onHide={() => true} showCloseIcon={false}>
@@ -16,26 +27,26 @@ export default function SideNav() {
           />
         </div>
         <div>
-          <Link to="/device">
+          <Link to="/device" className="link">
             <Button
-              label="device"
-              className="p-button-warning m-1 mb-4 sidenav-buttons"
+              label="Device"
+              className={active[0]}
             />
           </Link>
         </div>
         <div>
-          <Link to="/company">
+          <Link to="/company" className="link">
             <Button
-              label="company"
-              className="p-button-warning m-1 mb-4 sidenav-buttons"
+              label="Company"
+              className={active[1]}
             />
           </Link>
         </div>
         <div>
-          <Link to="/search">
+          <Link to="/search" className="link">
             <Button
-              label="search"
-              className="p-button-warning m-1 mb-4 sidenav-buttons"
+              label="Search"
+              className={active[2]}
             />
           </Link>
         </div>
