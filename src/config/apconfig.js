d5c3e7ddd467e4def5f453c8f55fc11d5c864e00
Vimal Krishnan <akvimal@gmail.com>
2021-01-06 20:35:54 +0530

    added configuration for deployment

--- src/config/apconfig.js
+++ src/config/apconfig.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:5000" || process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Accept: "application/json",
   },
