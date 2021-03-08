4d5d307bb04729ebf95fba4c9d97472224fd61d8
Akash Immanuel <akashimmanuel2@gmail.com>
2021-03-08 16:29:29 +0530

    refactored added services and removed unwanted files

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 import dataCard from "./dataCard/reducer";
-import table from "./table/reducer"
+import table from "./table/reducer";
 
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
 
 const rootReducer = combineReducers({
   dataCard,
-  table
+  table,
 });
 
 export const store = createStoreWithMiddleware(rootReducer);
